67dee24c3bd177847c0a4423ca3ebd841968bd10
Blue Sugar <blue620sugar@gmail.com>
2025-01-20 06:59:23 +0900

    add dual segtree

--- data_structure/segment_tree/src/lib.rs
+++ data_structure/segment_tree/src/lib.rs
@@ -1,56 +1,58 @@
+pub mod dual_segment_tree;
+
 pub trait Monoid {
-    type Monoid: Copy;
-    fn e() -> Self::Monoid;
-    fn op(lhs: &Self::Monoid, rhs: &Self::Monoid) -> Self::Monoid;
+    type Value: Copy;
+    fn e() -> Self::Value;
+    fn op(lhs: &Self::Value, rhs: &Self::Value) -> Self::Value;
 }
 
 use std::ops::RangeBounds;
-pub struct SegmentTree<M: Monoid> {
-    values: Vec<M::Monoid>,
+pub struct SegmentTree<T: Monoid> {
+    values: Vec<T::Value>,
 }
-impl<M: Monoid> SegmentTree<M> {
-    pub fn new(a: &[M::Monoid]) -> Self {
+impl<T: Monoid> SegmentTree<T> {
+    pub fn new(a: &[T::Value]) -> Self {
         let n = a.len().next_power_of_two();
-        let mut values = vec![M::e(); 2 * n];
+        let mut values = vec![T::e(); 2 * n];
         values[n..n + a.len()].clone_from_slice(a);
         for i in (1..n).rev() {
-            values[i] = M::op(&values[2 * i], &values[2 * i + 1]);
+            values[i] = T::op(&values[2 * i], &values[2 * i + 1]);
         }
         Self { values }
     }
 
-    pub fn get_at(&self, idx: usize) -> M::Monoid {
+    pub fn get_at(&self, idx: usize) -> T::Value {
         self.values[idx + self.values.len() / 2]
     }
 
-    pub fn update_at(&mut self, i: usize, x: M::Monoid) {
+    pub fn update_at(&mut self, i: usize, x: T::Value) {
         let mut i = i + self.values.len() / 2;
         self.values[i] = x;
         i >>= 1;
         while i > 0 {
-            self.values[i] = M::op(&self.values[2 * i], &self.values[2 * i + 1]);
+            self.values[i] = T::op(&self.values[2 * i], &self.values[2 * i + 1]);
             i >>= 1;
         }
     }
 
-    pub fn fold<R: RangeBounds<usize>>(&self, range: R) -> M::Monoid {
+    pub fn fold<R: RangeBounds<usize>>(&self, range: R) -> T::Value {
         let (l, r) = unzip(range, self.values.len() / 2);
         let (mut l, mut r) = (l + self.values.len() / 2, r + self.values.len() / 2);
-        let mut left = M::e();
-        let mut right = M::e();
+        let mut left = T::e();
+        let mut right = T::e();
         while l < r {
             if l % 2 == 1 {
-                left = M::op(&left, &self.values[l]);
+                left = T::op(&left, &self.values[l]);
                 l += 1;
             }
             if r % 2 == 1 {
                 r -= 1;
-                right = M::op(&self.values[r], &right);
+                right = T::op(&self.values[r], &right);
             }
             l >>= 1;
             r >>= 1;
         }
-        M::op(&left, &right)
+        T::op(&left, &right)
     }
 }
 
