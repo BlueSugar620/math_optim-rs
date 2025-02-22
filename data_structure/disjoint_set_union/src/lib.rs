f5e7bb3a431afb85fc7c76a3cc9b77f18292a023
BlueSugar <emerald620blue@gmail.com>
2025-02-22 09:33:21 +0900

    add disjoint set union

---
+++ data_structure/disjoint_set_union/src/lib.rs
@@ -0,0 +1,48 @@
+pub mod potential_dsu;
+
+pub struct DisjointSetUnion {
+    parents: Vec<isize>,
+    cnt: usize,
+}
+
+impl DisjointSetUnion {
+    pub fn new(n: usize) -> Self {
+        Self {
+            parents: vec![-1; n],
+            cnt: n,
+        }
+    }
+
+    pub fn root(&self, mut v: usize) -> usize {
+        while self.parents[v] >= 0 {
+            v = self.parents[v] as usize;
+        }
+        v
+    }
+
+    pub fn unite(&mut self, u: usize, v: usize) {
+        let mut u = self.root(u);
+        let mut v = self.root(v);
+        if u == v {
+            return;
+        }
+        if self.parents[u] > self.parents[v] {
+            std::mem::swap(&mut u, &mut v);
+        }
+        self.parents[u] += self.parents[v];
+        self.parents[v] = u as isize;
+        self.cnt -= 1;
+    }
+
+    pub fn is_same(&self, u: usize, v: usize) -> bool {
+        self.root(u) == self.root(v)
+    }
+
+    pub fn size(&self, v: usize) -> usize {
+        -self.parents[self.root(v)] as usize
+    }
+
+    pub fn cnt(&self) -> usize {
+        self.cnt
+    }
+}
---
+++ data_structure/disjoint_set_union/src/potential_dsu.rs
@@ -0,0 +1,68 @@
+pub trait Group {
+    type Value: Clone + PartialEq;
+    fn id() -> Self::Value;
+    fn mul(lhs: &Self::Value, rhs: &Self::Value) -> Self::Value;
+    fn inv(val: &Self::Value) -> Self::Value;
+}
+
+pub struct PotentialDSU<T: Group> {
+    parents: Vec<isize>,
+    potentials: Vec<T::Value>,
+    cnt: usize,
+}
+
+impl<T: Group> PotentialDSU<T> {
+    pub fn new(n: usize) -> Self {
+        Self {
+            parents: vec![-1; n],
+            potentials: vec![T::id(); n],
+            cnt: n,
+        }
+    }
+
+    pub fn root(&self, mut v: usize) -> (usize, T::Value) {
+        let mut potential = self.potentials[v].clone();
+        while self.parents[v] >= 0 {
+            v = self.parents[v] as usize;
+            potential = T::mul(&self.potentials[v], &potential);
+        }
+        (v, potential)
+    }
+
+    pub fn unite(&mut self, from: usize, to: usize, d: T::Value) -> bool {
+        let (mut from, p_from) = self.root(from);
+        let (mut to, p_to) = self.root(to);
+        if from == to {
+            T::mul(&p_from, &d) == p_to
+        } else {
+            let mut d = T::mul(&T::mul(&p_from, &d), &T::inv(&p_to));
+            if self.parents[from] > self.parents[to] {
+                std::mem::swap(&mut from, &mut to);
+                d = T::inv(&d);
+            }
+            self.parents[from] += self.parents[to];
+            self.parents[to] = from as isize;
+            self.potentials[to] = d;
+            self.cnt -= 1;
+            true
+        }
+    }
+
+    pub fn poteintial(&self, from: usize, to: usize) -> Option<T::Value> {
+        let (from, p_from) = self.root(from);
+        let (to, p_to) = self.root(to);
+        if from == to {
+            Some(T::mul(&T::inv(&p_from), &p_to))
+        } else {
+            None
+        }
+    }
+
+    pub fn size(&self, u: usize) -> usize {
+        -self.parents[self.root(u).0] as usize
+    }
+
+    pub fn cnt(&self) -> usize {
+        self.cnt
+    }
+}
