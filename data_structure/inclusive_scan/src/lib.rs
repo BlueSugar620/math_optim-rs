6fde920ae90d804ac1c979c0adf05ff07ebdf261
BlueSugar <emerald620blue@gmail.com>
2025-02-22 09:40:29 +0900

    add inclusive scan

---
+++ data_structure/inclusive_scan/src/lib.rs
@@ -0,0 +1,45 @@
+use std::ops::{Bound, RangeBounds};
+
+pub trait Group {
+    type Value: Clone;
+    fn id() -> Self::Value;
+    fn mul(lhs: &Self::Value, rhs: &Self::Value) -> Self::Value;
+    fn inv(val: &Self::Value) -> Self::Value;
+}
+
+pub struct InclusiveScan<T: Group> {
+    scan: Vec<T::Value>,
+}
+
+impl<T: Group> InclusiveScan<T> {
+    pub fn new(a: &[T::Value]) -> Self {
+        Self {
+            scan: std::iter::once(T::id())
+                .chain(a.to_vec())
+                .scan(T::id(), |acc, a| {
+                    *acc = T::mul(acc, &a);
+                    Some(acc.clone())
+                })
+                .collect::<Vec<_>>(),
+        }
+    }
+
+    pub fn fold<R: RangeBounds<usize>>(&self, range: R) -> T::Value {
+        let (l, r) = unzip(range, self.scan.len() - 1);
+        T::mul(&T::inv(&self.scan[l]), &self.scan[r])
+    }
+}
+
+fn unzip<R: RangeBounds<usize>>(range: R, n: usize) -> (usize, usize) {
+    let start = match range.start_bound() {
+        Bound::Unbounded => 0,
+        Bound::Included(&x) => x,
+        Bound::Excluded(&x) => x + 1,
+    };
+    let end = match range.end_bound() {
+        Bound::Unbounded => n,
+        Bound::Included(&x) => x + 1,
+        Bound::Excluded(&x) => x,
+    };
+    (start, end)
+}
