f379e17f661ac89f17679cb8f79924cc7e7a9c7c
BlueSugar <emerald620blue@gmail.com>
2025-03-14 20:37:03 +0900

    update fps

--- fps/convolution/src/bitwise.rs
+++ fps/convolution/src/bitwise.rs
@@ -1,63 +1,64 @@
-pub trait BitOp {
-    type Value: Copy;
-    fn mul(a: &Self::Value, b: &Self::Value) -> Self::Value;
-    fn sqrt_coef(a: &mut Self::Value);
-    fn representation_matrix(a: &Self::Value, b: &Self::Value) -> (Self::Value, Self::Value);
-}
-
-pub fn bitwise_convolution<T: BitOp>(a: &[T::Value], b: &[T::Value]) -> Vec<T::Value> {
-    let mut a = a.to_vec();
-    let mut b = b.to_vec();
-    bitwise_transform::<T>(&mut a);
-    bitwise_transform::<T>(&mut b);
-    let mut c = a
-        .iter()
-        .zip(b.iter())
-        .map(|(a, b)| T::mul(a, b))
-        .collect::<Vec<_>>();
-    bitwise_transform::<T>(&mut c);
-    let k = (c.len() - 1).count_ones();
-    c.iter_mut().for_each(|c| {
-        for _ in 0..k {
-            T::sqrt_coef(c);
-        }
-    });
-    c
-}
-
-fn bitwise_transform<T: BitOp>(a: &mut [T::Value]) {
-    let n = a.len();
-    let mut k = 1;
-    while k < n {
-        for i in (0..n).step_by(2 * k) {
-            for j in i..i + k {
-                (a[j], a[j + k]) = T::representation_matrix(&a[j], &a[j + k]);
-            }
-        }
-        k <<= 1;
-    }
-}
+use super::Convolution;
+use galois_field::GF;
 
 const MOD: u64 = 998_244_353;
 
-enum Xor {}
-impl BitOp for Xor {
-    type Value = u64;
-    fn mul(a: &Self::Value, b: &Self::Value) -> Self::Value {
-        a * b % MOD
+pub enum Xor {}
+impl BitConv for Xor {
+    fn fourier_matrix(lhs: &mut GF<MOD>, rhs: &mut GF<MOD>) {
+        (*lhs, *rhs) = (*lhs + *rhs, *lhs - *rhs);
     }
-    fn sqrt_coef(a: &mut Self::Value) {
-        if *a & 1 == 0 {
-            *a = *a >> 1;
-        } else {
-            *a = (*a + MOD) >> 1;
+    fn inverse_matrix(lhs: &mut GF<MOD>, rhs: &mut GF<MOD>) {
+        (*lhs, *rhs) = (*lhs + *rhs, *lhs - *rhs);
+    }
+}
+
+use std::marker::PhantomData;
+pub trait BitConv {
+    fn fourier_matrix(lhs: &mut GF<MOD>, rhs: &mut GF<MOD>);
+    fn inverse_matrix(lhs: &mut GF<MOD>, rhs: &mut GF<MOD>);
+}
+
+struct Bitwise<T: BitConv> {
+    phantom: PhantomData<T>,
+}
+impl<T: BitConv> Convolution for Bitwise<T> {
+    type Value = GF<MOD>;
+    fn e() -> Self::Value {
+        GF::new(0)
+    }
+    fn mul(lhs: &Self::Value, rhs: &Self::Value) -> Self::Value {
+        lhs * rhs
+    }
+    fn fourier_transform(a: &mut [Self::Value]) {
+        let n = a.len();
+        let b = n.trailing_zeros() as usize;
+        for k in 0..b {
+            let k = 1 << k;
+            for a in a.chunks_exact_mut(2 * k) {
+                let (x, y) = a.split_at_mut(k);
+                for (x, y) in x.iter_mut().zip(y.iter_mut()) {
+                    T::fourier_matrix(x, y);
+                }
+            }
         }
     }
-    fn representation_matrix(a: &Self::Value, b: &Self::Value) -> (Self::Value, Self::Value) {
-        let x = a + b;
-        (
-            if x >= MOD { x - MOD } else { x },
-            if a < b { a + MOD - b } else { a - b },
-        )
+    fn inverse_transform(a: &mut [Self::Value]) {
+        let n = a.len();
+        let b = n.trailing_zeros() as usize;
+        for k in 0..b {
+            let k = 1 << k;
+            for a in a.chunks_exact_mut(2 * k) {
+                let (x, y) = a.split_at_mut(k);
+                for (x, y) in x.iter_mut().zip(y.iter_mut()) {
+                    T::inverse_matrix(x, y);
+                }
+            }
+        }
+
+        let coef = GF::new(2).inv().pow(b as u64);
+        for a in a.iter_mut() {
+            *a *= coef;
+        }
     }
 }
